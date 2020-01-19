151a6dca1e622c43cac053750fb400c0965d8286
Miyamura80 <eitomiyamura@gmail.com>
2020-01-19 04:11:18 +0000

    Commiting the initial finish of the recipe program

--- dataParser.py
+++ dataParser.py
@@ -1,31 +1,82 @@
 import spoonacular as sp
 api = sp.API("ec9eb3bb895e4078bb61b9e7b9cf05e7")
 
-with open("exampleFridgeContent.txt","r") as f:
-    data = f.readlines()
-
-contents = {}
-
-for rawData in data:
-    dataExtract = api.parse_ingredients(rawData.strip("\n")).json()[0]
-    contents[dataExtract['name']] = [dataExtract['amount'],dataExtract['unitLong']]
+unwantedUnits = ['large', 'larges', 'serving', 'servings']
 
 
-print(contents)
+def loadFridgeContent():
+    with open("exampleFridgeContent.txt","r") as f:
+        data = f.readlines()
+
+    contents = {}
+
+    for rawData in data:
+        spl = rawData.split("$")
+        contName = spl[0]
+        contAmount = spl[1]
+        contUnit = spl[2]
+        contents[contName] = [contAmount,contUnit]
+    return contents
 
 
-ingList = ""
-for item in contents:
-    print(item)
-    ingList += item + ","
+def generateRecipes(contents):
+    ingList = ""
+    for item in contents:
+        ingList += item + ","
+    ingList = ingList[0:(len(ingList)-2)]
+    recipesJsonList = api.search_recipes_by_ingredients(ingredients=ingList,number=5,ranking=1).json()
 
-ingList = ingList[0:(len(ingList)-2)]
-
-print(ingList)
-
-suggestion = api.search_recipes_by_ingredients(ingredients=ingList,number=1,ranking=1).json()
-print(suggestion)
+    recipes = []
 
 
+    for recipeJson in recipesJsonList:
+        id = recipeJson['id']
+        name = recipeJson['title']
+        usedIngrJsonList = recipeJson['usedIngredients']
+        recipeLink = api.get_recipe_information(id).json()['sourceUrl']
+        ingInfoList = []
+        for ingrd in usedIngrJsonList:
+            ingrdUnit = ingrd['unitLong']
+            ingrdName = ingrd['name']
+            ingrdAmount = ingrd['amount']
+            ingInfoList.append({'name':ingrdName,'amount':ingrdAmount,'unitLong':ingrdUnit})
+        recipes.append({'name': name, 'link': recipeLink,'ingList':ingInfoList})
+    return recipes
 
 
+def promptRecipe():
+    contents = loadFridgeContent()
+    recipes = generateRecipes(contents)
+    print("Recommended recipes:")
+    for i in range(len(recipes)):
+        print((str(i+1)+"."),recipes[i]['name'],"-",recipes[i]['link'])
+
+    choose = int(input("Enter choice (Enter number between 1-5): "))
+    print(recipes[choose-1]['name'],"-",recipes[choose-1]['link'])
+
+
+
+def takeOutFromFridge(ingList):
+    with open("exampleFridgeContent.txt", "r") as f:
+        data = f.readlines()
+    splitted = map((lambda x : x.split("$")), data)
+
+    names = [splitted[i][0] for i in range(len(data))]
+
+    for ing in ingList:
+        ingN = ing['name']
+        ingAmt = ing['amount']
+        intUnt = ing['unitLong']
+        if ingN in names:
+            pass
+
+    f = open("exampleFridgeContent.txt","w")
+
+
+    f.close()
+
+
+
+
+
+promptRecipe()
--- recipe.py
+++ recipe.py
@@ -2,4 +2,4 @@ import spoonacular as sp
 api = sp.API("ec9eb3bb895e4078bb61b9e7b9cf05e7")
 
 
-
+api.
