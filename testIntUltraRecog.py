37a7b427ae1422470ec1f7e3578484218cd15569
Miyamura80 <eitomiyamura@gmail.com>
2020-01-19 10:10:23 +0000

    Finished the pipeline for image rec to fridge content

--- testIntUltraRecog.py
+++ testIntUltraRecog.py
@@ -3,18 +3,14 @@ from azure.cognitiveservices.vision.computervision.models import TextOperationSt
 from azure.cognitiveservices.vision.computervision.models import TextRecognitionMode
 from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
 from msrest.authentication import CognitiveServicesCredentials
-
 import spoonacular as sp
-api = sp.API("ec9eb3bb895e4078bb61b9e7b9cf05e7")
-
+api = sp.API("32705ed053d745d3b9cfe60f248d1ee4")
 from picamera import PiCamera
 from time import sleep
-
 #setting up camera
 camera = PiCamera()
-camera.resolution = (1024, 600)
+camera.resolution = (1800,1200)
 camera.framerate = 15
-
 import asyncio, io, glob, os, sys, time, uuid, requests
 from array import array
 import os
@@ -39,22 +35,17 @@ else:
 #print (endpoint)
 computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
 #remote_image_url = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/landmark.jpg"
-
 #userProfile = os.environ['userprofile']
 imagePath = "/home/pi/Desktop/image.jpg"
 #ultSonic.calculateDistance()
-
-def putInFridge(descr):
+def putInFridge(namePara):
     with open("exampleFridgeContent.txt", "r") as f:
         data = f.readlines()
-
-
     indexFound = -1
     for i in range(len(data)):
         ingN = data[i].split('$')[0]
-        if ingN.lower()==name.lower():
+        if ingN.lower()==namePara.lower():
             indexFound = i
-
     f = open("exampleFridgeContent.txt","w")
     for i in range(len(data)):
         if i == indexFound:
@@ -64,12 +55,10 @@ def putInFridge(descr):
             f.write(split[0]+'$'+str(newAmount)+'$'+split[2])
         else:
             f.write(data[i])
-
-    if f==-1:
-        f.write(name+'$'+str(1)+'$')
+    print(indexFound)
+    if indexFound==-1:
+        f.write('\n'+namePara+'$'+str(1)+'$')
     f.close()
-
-
 while True:
   #ultSonic.calculateDistance()
   if(ultSonic.calculateDistance() <= 10):
@@ -84,18 +73,19 @@ while True:
     print("===== Describe an image - remote =====")
     # Call API
     description_results = computervision_client.describe_image_in_stream(image=buffer)
-
-
     print("Description of remote image: ")
     if (len(description_results.captions) == 0):
       print("No description detected.")
     else:
       for caption in description_results.captions:
         print("'{}' with confidence {:.2f}%".format(caption.text, caption.confidence * 100))
-        name = api.detect_food_in_text(caption.text).json()['annotations'][0]['annotation'] != ""
-        if caption.confidence > 0.5 and name!="":
-            putInFridge(name)
-            print("New entry added to the fridge")
+        print(api.detect_food_in_text(caption.text).json())
+        name = api.detect_food_in_text(caption.text).json()['annotations']
+        if len(name)!=0:
+        	name2 = name[0]['annotation']
+	        if name2!="":
+	            print(name2)
+	            putInFridge(name2)
+	            print("New entry added to the fridge")
     sleep(2)
-
 
