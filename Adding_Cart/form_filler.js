6d4132c37322b117999fedb529dacc642f4e92f9
Kah Gene Leong <kgleong2001@gmail.com>
2020-01-19 03:26:02 +0000

    Fixed some errors

--- Adding_Cart/form_filler.js
+++ Adding_Cart/form_filler.js
@@ -3,15 +3,17 @@ window.onload = function(){
     xmlhttp.onreadystatechange = function(){
     if(xmlhttp.status==200 && xmlhttp.readyState==4){    
         var words = xmlhttp.responseText.split('$');
+        j=1
         for (i = 1; i<5; i+=3){
-            document.getElementById('A'.concat(i)).value=(words[i]);
-            document.getElementById('Q'.concat(i)).value=(1);
+            document.getElementById('A'.concat(j)).value=(words[i]);
+            document.getElementById('Q'.concat(j)).value=(1);
+            j+=1
         }
     }
     }
-    xmlhttp.open("GET","https://raw.githubusercontent.com/Miyamura80/CambridgeHackathon2020/master/Shopping_List.txt",true);
+    xmlhttp.open("GET","https://raw.githubusercontent.com/Miyamura80/CambridgeHackathon2020/master/Adding_Cart/Shopping_List.txt",true);
     xmlhttp.send();
     
     var button = document.getElementById('clickButton');
-    setTimeout(function(){button.form.submit();}, 300);
+    //setTimeout(function(){button.form.submit();}, 300);
     }
